d3e934df13425c98e776ff0d2bfd90360240b5ad
Rohit Ghatol <rohit.ghatol@synerzip.com>
2013-05-22 19:24:36 -0500

    Added files

--- web.js
+++ web.js
@@ -1,6 +1,7 @@
 var express = require("express");
 var app = express();
 app.use(express.logger());
+app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(request, response) {
   response.send('Hello World!');
@@ -10,3 +11,5 @@ var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
+
+
